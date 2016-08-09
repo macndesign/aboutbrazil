9cd964b9fcbdd280107c1d91c9cc281b2e02a0cb
Mário Chaves <macndesign@gmail.com>
2016-08-09 01:56:48 -0300

    cmt

--- send.sh
+++ send.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+rm -fR build
+npm run build
 aws s3 rm s3://aboutbrazil/* --recursive
 aws s3 cp build s3://aboutbrazil/ --recursive
 aws s3 cp assets s3://aboutbrazil/assets --recursive
-
+aws s3 cp data s3://aboutbrazil/data --recursive
