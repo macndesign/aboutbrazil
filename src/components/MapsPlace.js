f762a69345d1896ffd630acccdf5757627e02935
Mário Chaves <macndesign@gmail.com>
2016-09-16 08:20:17 -0300

    new maps marker

--- src/components/MapsPlace.js
+++ src/components/MapsPlace.js
@@ -1,24 +1,16 @@
 import React, {Component} from 'react';
 
 const markerStyle = {
-    background: '#c62828',
-    height: 32,
-    width: 32,
-    border: 'solid 5px #ef5350',
-    borderRadius: 16,
-    opacity: 0.9,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'center',
-    paddingTop: 4
+    height: 64,
+    width: 64,
+    backgroundImage: "url('./assets/images/maps-marker-1.png')",
+    backgroundSize: 'cover'
 };
 
 class MapsPlace extends Component {
     render() {
         return (
-            <div style={markerStyle}>
-                {this.props.text}
-            </div>
+            <div style={markerStyle} title={this.props.text}/>
         );
     }
 }
